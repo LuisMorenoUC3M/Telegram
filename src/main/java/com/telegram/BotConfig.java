ffd405a50a1ff04adc8b99c7351c49a167936649
lmmor <lmmor@DESKTOP-SSK8T2F>
2017-04-03 18:06:25 +0200

    .

--- src/main/java/com/telegram/BotConfig.java
+++ src/main/java/com/telegram/BotConfig.java
@@ -10,30 +10,4 @@ public class BotConfig {
     public static final String REPITEME_TOKEN = "281417678:AAE5a0HRwoZJoGYaW0LeY1LJor6ZJHSxxnI";
     public static final String REPITEME_USER = "repitemebot";
     public static final String GOOGLE_API_KEY = "AIzaSyCLdMyfRVcnHrtZqB2oCHdYlqEs3LgamvQ";
-	
-    /**
-	public static final String WEATHER_TOKEN = "<token>";
-    public static final String WEATHER_USER = "weatherbot";
-
-    public static final String TRANSIFEX_TOKEN = "<token>";
-    public static final String TRANSIFEX_USER = "TGlanguagesbot";
-
-    public static final String FILES_TOKEN = "<token>";
-    public static final String FILES_USER = "filesbot";
-
-    public static final String DIRECTIONS_TOKEN = "<token>";
-    public static final String DIRECTIONS_USER = "directionsbot";
-
-    public static final String CHANNEL_TOKEN = "<token>";
-    public static final String CHANNEL_USER = "channelupdatesbot";
-
-    public static final String RAE_TOKEN = "<token>";
-    public static final String RAE_USER = "raebot";
-
-    public static final String WEBHOOK_TOKEN = "<token>";
-    public static final String WEBHOOK_USER = "webhooksamplebot";
-
-    public static final String COMMANDS_TOKEN = "<token>";
-    public static final String COMMANDS_USER = "MyCommandsBot";
-    **/
 }
